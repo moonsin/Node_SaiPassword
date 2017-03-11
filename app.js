1f8d3a1b62839221ec05617a2381cedbb9a940f2
moon <moonsin@outlook.com>
2017-03-12 02:18:48 +0800

    优化代码，完成注册验证

--- app.js
+++ app.js
@@ -1,42 +1,53 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
-
-app.use(bodyParser.urlencoded({
-    extended: false
-}));
-app.use(bodyParser.json());
-//  主页输出 "Hello World"
-
-//  POST 请求
-app.post('/signup', function(req, res) {
-    console.log(req.body.userId);
-    console.log("主页 POST 请求");
-    var mysql = require('mysql');
-    var connection = mysql.createConnection({
+var dateBaseInfo = {
         host: 'localhost',
         user: 'root',
         password: '',
         database: 'SaiPassword'
-    });
+    };
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+app.use(bodyParser.json());
+var mysql = require('mysql');
 
+//  POST 请求
+app.post('/signup', function(req, res) {
+    function findUserId(userId) {
+        let $sql = 'SELECT * FROM user WHERE user_name ="' + userId + '"';
+        connection.query($sql, function(err, rows, fields) {
+            if (err) throw err;
+            var exist = !!rows.length;
+            console.log(exist);
+            if (!exist) {
+                insertUser();
+            } else {
+                connection.end();
+            }
+            res.send({
+                userExist: exist
+            });
+        });
+    }
+    function insertUser() {
+        var $sql = 'INSERT INTO user(user_name,password,email,submission_date) VALUES("' + req.body.userId + '","' + req.body.password + '","' + req.body.email + '",NOW())';
+        console.log($sql);
+        connection.query($sql, function(err, rows, fields) {
+            if (err) throw err;
+            console.log('insert successfully ');
+            connection.end();
+        });
+
+    }
+    var connection = mysql.createConnection(dateBaseInfo);
     connection.connect();
-    console.log(req.body.email);
-    var $sql = 'INSERT INTO user(user_name,password,email,submission_date) VALUES("'+req.body.userId+'","'+req.body.password+'","'+req.body.email+'",NOW())';
-    console.log($sql);
-    connection.query($sql, function(err, rows, fields) {
-        if (err) throw err;
-        console.log('insert successfully ', rows);
-    });
-
-    connection.end();
-    res.send('Hello POST');
+    findUserId(req.body.userId);
 })
 var server = app.listen(8080, function() {
-
     var host = server.address().address
     var port = server.address().port
-
     console.log("应用实例，访问地址为 http://%s:%s", host, port)
 
 })
