97f981e053ea93888d934791f4806ede20de2900
moon <moonsin@outlook.com>
2017-03-20 17:25:29 +0800

    通过async控制sql流程

--- common/sqlApi.js
+++ common/sqlApi.js
@@ -4,29 +4,29 @@ var bodyParser = require('body-parser');
 var router = express.Router();
 var dataBase = require('../common/dataBase');
 var sqlApi = require('../common/sqlApi');
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({
+	extended: false
+}));
 app.use(bodyParser.json());
 
 
 
-var ifUserIdExist = function (userId,connection) {
-    let $sql = 'SELECT * FROM user WHERE user_name ="' + userId + '"';
-    connection.query($sql, function(err, rows, fields) {
-        if (err) throw err;
-        var exist = !!rows.length;
-        return exist;
-    });
+var selectUset = function(connection, $sql, callback) {
+	connection.query($sql, function(err, rows, fields) {
+		if (err) throw err;
+		callback(null, !!rows.length);
+	});
 }
 
-function insertUser(userId,password,email,connection) {
-    var $sql = 'INSERT INTO user(user_name,password,email,submission_date) VALUES("' + userId + '","' + password + '","' + email + '",NOW())';
-    console.log($sql);
-    connection.query($sql, function(err, rows, fields) {
-        if (err) throw err;
-        console.log('insert successfully ');
-    });
+function insertUser(connection,$sql,callback) {
+	console.log($sql);
+	connection.query($sql, function(err, rows, fields) {
+		if (err) throw err;
+		console.log('insert successfully ');
+	    callback(null, false);
+	});
 
 }
 
-exports.ifUserIdExist = ifUserIdExist;
+exports.selectUset = selectUset;
 exports.insertUser = insertUser;
--- routes/router.js
+++ routes/router.js
@@ -2,30 +2,57 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var router = express.Router();
-var {dataBaseInfo} = require('../common/dataBase');
+var {
+	dataBaseInfo
+} = require('../common/dataBase');
 var sqlApi = require('../common/sqlApi');
 var mysql = require('mysql');
-app.use(bodyParser.urlencoded({extended: false}));
+var async = require('async');
+app.use(bodyParser.urlencoded({
+	extended: false
+}));
 app.use(bodyParser.json());
 
 //  主页输出 "Hello World"
-router.get('/', function (req, res) {
-   console.log("主页 GET 请求");
-   res.send('test router');
+router.get('/', function(req, res) {
+	console.log("主页 GET 请求");
+	res.send('test router');
 })
 
 router.post('/signup', function(req, res) {
-   var connection = mysql.createConnection(dataBaseInfo);
-   connection.connect();
-   var userIdExist = sqlApi.ifUserIdExist(req.body.userId,connection);
-   if(!userIdExist){
-       sqlApi.insertUser(req.body.userId,req.body.password,req.body.email,connection); 
-   }
-   connection.end();
-    res.send({
-        userExist: userIdExist
-    });
+	var connection = mysql.createConnection(dataBaseInfo);
+	var sqls = {
+		'selectSQL': 'SELECT * FROM user WHERE user_name ="' + req.body.userId + '"',
+		'insertSQL': 'INSERT INTO user(user_name,password,email,submission_date) VALUES("' + req.body.userId + '","' + req.body.password + '","' + req.body.email + '",NOW())',
+	}
 
+	connection.connect();
+	async.waterfall([
+		function(callback) {
+			sqlApi.selectUset(connection, sqls.selectSQL, callback);
+		},
+		function(existState, callback) {
+			if (!existState) {
+				sqlApi.insertUser(connection, sqls.insertSQL, callback);
+			} else {
+				callback(null, true)
+			}
+		},
+		function(existState, callback) {
+			connection.end();
+			res.send({
+				userExist: existState
+			});
+			callback(null, 'done');
+		}
+	], function(err, result) {
+		// result now equals 'done'
+		if (err) {
+			console.log(err);
+		} else {
+			console.log('c:' + result)
+		}
+	});
 
 })
 
