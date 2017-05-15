ed1bb2da4a27f18e68c657b663f2dd92ee019862
moon <moonsin@outlook.com>
2017-05-15 13:12:57 +0800

    完成退出登录以及登录功能接口

--- common/sqlApi.js
+++ common/sqlApi.js
@@ -5,28 +5,48 @@ var router = express.Router();
 var dataBase = require('../common/dataBase');
 var sqlApi = require('../common/sqlApi');
 app.use(bodyParser.urlencoded({
-	extended: false
+    extended: false
 }));
 app.use(bodyParser.json());
 
-
-
 var selectUset = function(connection, $sql, callback) {
-	connection.query($sql, function(err, rows, fields) {
-		if (err) throw err;
-		callback(null, !!rows.length);
-	});
+    connection.query($sql, function(err, rows, fields) {
+        if (err) throw err;
+        callback(null, !!rows.length);
+    });
 }
 
-function insertUser(connection,$sql,callback) {
-	console.log($sql);
-	connection.query($sql, function(err, rows, fields) {
-		if (err) throw err;
-		console.log('insert successfully ');
-	    callback(null, false);
-	});
+function insertUser(connection, $sql, callback) {
+    console.log($sql);
+    connection.query($sql, function(err, rows, fields) {
+        if (err) throw err;
+        console.log('insert successfully ');
+        callback(null, false);
+    });
 
+}
+
+function updateUser(connection, $sql, callback) {
+    console.log($sql);
+    connection.query($sql, function(err, rows, fields) {
+        if (err) throw err;
+        console.log('update successfully ');
+        callback(null, true);
+    });
+}
+var selectUserPassword = function(connection, $sql, callback) {
+    connection.query($sql, function(err, rows, fields) {
+        if (err) throw err;
+        console.log(rows[0]);
+        if (rows[0]) {
+            callback(null, rows[0]);
+        } else {
+            callback(null, false);
+        }
+    });
 }
 
 exports.selectUset = selectUset;
 exports.insertUser = insertUser;
+exports.updateUser = updateUser;
+exports.selectUserPassword = selectUserPassword;
--- routes/router.js
+++ routes/router.js
@@ -3,56 +3,140 @@ var app = express();
 var bodyParser = require('body-parser');
 var router = express.Router();
 var {
-	dataBaseInfo
+    dataBaseInfo
 } = require('../common/dataBase');
 var sqlApi = require('../common/sqlApi');
 var mysql = require('mysql');
 var async = require('async');
 app.use(bodyParser.urlencoded({
-	extended: false
+    extended: false
 }));
 app.use(bodyParser.json());
 
 //  主页输出 "Hello World"
 router.get('/', function(req, res) {
-	console.log("主页 GET 请求");
-	res.send('test router');
+    console.log("主页 GET 请求");
+    res.send('test router');
 })
 
 router.post('/signup', function(req, res) {
-	var connection = mysql.createConnection(dataBaseInfo);
-	var sqls = {
-		'selectSQL': 'SELECT * FROM user WHERE user_name ="' + req.body.userId + '"',
-		'insertSQL': 'INSERT INTO user(user_name,password,email,submission_date) VALUES("' + req.body.userId + '","' + req.body.password + '","' + req.body.email + '",NOW())',
-	}
+    var connection = mysql.createConnection(dataBaseInfo);
+    var sqls = {
+        'selectSQL': 'SELECT * FROM user WHERE user_name ="' + req.body.userId + '"',
+        'insertSQL': 'INSERT INTO user(user_name,password,email,submission_date) VALUES("' + req.body.userId + '","' + req.body.password + '","' + req.body.email + '",NOW())',
+    }
 
-	connection.connect();
-	async.waterfall([
-		function(callback) {
-			sqlApi.selectUset(connection, sqls.selectSQL, callback);
-		},
-		function(existState, callback) {
-			if (!existState) {
-				sqlApi.insertUser(connection, sqls.insertSQL, callback);
-			} else {
-				callback(null, true)
-			}
-		},
-		function(existState, callback) {
-			connection.end();
-			res.send({
-				userExist: existState
-			});
-			callback(null, 'done');
-		}
-	], function(err, result) {
-		// result now equals 'done'
-		if (err) {
-			console.log(err);
-		} else {
-			console.log('c:' + result)
-		}
-	});
+    connection.connect();
+    async.waterfall([
+        function(callback) {
+            sqlApi.selectUset(connection, sqls.selectSQL, callback);
+        },
+        function(existState, callback) {
+            if (!existState) {
+                sqlApi.insertUser(connection, sqls.insertSQL, callback);
+            } else {
+                callback(null, true)
+            }
+        },
+        function(existState, callback) {
+            connection.end();
+            res.send({
+                userExist: existState
+            });
+            callback(null, 'done');
+        }
+    ], function(err, result) {
+        // result now equals 'done'
+        if (err) {
+            console.log(err);
+        } else {
+            console.log('c:' + result)
+        }
+    });
+
+})
+
+router.post('/logout', function(req, res) {
+    var connection = mysql.createConnection(dataBaseInfo);
+    var sqls = {
+        'updateSQL': 'UPDATE user SET data = \'' + req.body.data + '\' WHERE user_name =\'' + req.body.userId + '\'',
+    }
+
+    connection.connect();
+    async.waterfall([
+        function(callback) {
+            sqlApi.updateUser(connection, sqls.updateSQL, callback);
+        },
+        function(existState, callback) {
+            connection.end();
+            res.send({
+                logout: true
+            });
+            callback(null, 'done');
+        }
+    ], function(err, result) {
+        // result now equals 'done'
+        if (err) {
+            console.log(err);
+        } else {
+            console.log('c:' + result)
+        }
+    });
+
+})
+
+router.post('/firstLogin', function(req, res) {
+    var connection = mysql.createConnection(dataBaseInfo);
+    var sqls = {
+        'selectSQL': 'SELECT * FROM user WHERE user_name ="' + req.body.userId + '"',
+    }
+    connection.connect();
+    async.waterfall([
+        function(callback) {
+            sqlApi.selectUserPassword(connection, sqls.selectSQL, callback);
+        },
+        function(row, callback) {
+            connection.end();
+            res.send({
+                password: row.password
+            });
+            callback(null, 'done');
+        }
+    ], function(err, result) {
+        // result now equals 'done'
+        if (err) {
+            console.log(err);
+        } else {
+            console.log('c:' + result)
+        }
+    });
+
+})
+router.post('/getData', function(req, res) {
+    var connection = mysql.createConnection(dataBaseInfo);
+    var sqls = {
+        'selectSQL': 'SELECT * FROM user WHERE user_name ="' + req.body.userId + '" AND password = "' +req.body.password+'"',
+    }
+    connection.connect();
+    async.waterfall([
+        function(callback) {
+            sqlApi.selectUserPassword(connection, sqls.selectSQL, callback);
+        },
+        function(row, callback) {
+            connection.end();
+            res.send({
+                data: row.data
+            });
+            callback(null, 'done');
+        }
+    ], function(err, result) {
+        // result now equals 'done'
+        if (err) {
+            console.log(err);
+        } else {
+            console.log('c:' + result)
+        }
+    });
 
 })
 
